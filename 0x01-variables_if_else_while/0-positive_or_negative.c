1871a24228026d784f40ff2b63014c9ea93f6add
David Mensah <phormula4real@gmail.com>
2022-04-10 17:12:52 -0700

    fixing betty style coding

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,7 +6,7 @@
  * Description : Displays result of program
  * n: the random number
  * main - entry point
- * Return: 0
+ * Return: 0 on success
  */
 int main(void)
 {
@@ -15,12 +15,19 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n <0 )
+	if (n <0)
+	{
 		printf("%d is negative\n", n);
+	}
 	else if (n > 0)
+	{
 		printf("%d is positive\n", n);
+	}
 	else 
+	{
 		printf("%d is zero\n", n);
+	}
 	return (0);
+
 }
 
