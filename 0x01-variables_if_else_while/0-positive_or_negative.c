82fd7eb579be69afaeed3049a29edeca7cf5c8d7
David Mensah <phormula4real@gmail.com>
2022-04-10 17:02:15 -0700

    fixed betty style

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,22 +3,24 @@
 #include <stdio.h>
 /**
  * Programme is used to determine is a number is positive, negative or zero
- *
+ * Description : Displays result of program
+ * n: the random number
+ * main - entry point
  * Return: 0
  */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	if (n <0)
+	if (n <0 )
 		printf("%d is negative\n", n);
 	else if (n > 0)
 		printf("%d is positive\n", n);
 	else 
 		printf("%d is zero\n", n);
-
 	return (0);
 }
 
